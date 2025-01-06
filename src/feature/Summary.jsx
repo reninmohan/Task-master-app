928cd3786721f58d5db580366393a3435f4ad7c2
Renin <reninmohan17@gmail.com>
2025-01-06 23:06:45 +0530

    updated summary

--- src/feature/Summary.jsx
+++ src/feature/Summary.jsx
@@ -10,8 +10,14 @@ function Summary() {
       <div className="d-md-flex flex-wrap justify-content-md-center p-2 gap-md-4 d-sm-grid summary-grid-layout ">
         <SummaryComponent name="Today" noOfTask={24} color="#4487E6" accentColor="#000000" logo={<RiProgress5Line />} />
         <SummaryComponent name="All" noOfTask={24} color="#59C6E1" accentColor="#00191f" logo={<MdOutlineTimer />} />
-        <SummaryComponent name="Completed" noOfTask={24} color="#FFB300" accentColor="#3a2901" logo={<MdCheckCircleOutline />} />
-        <SummaryComponent name="Overdue" noOfTask={24} color="#FB5D5D" accentColor="#330101" logo={<MdOutlineErrorOutline />} />
+        <SummaryComponent
+          name="Completed"
+          noOfTask={24}
+          color="#FFB300"
+          accentColor="#3a2901"
+          logo={<MdCheckCircleOutline />}
+        />
+        {/* <SummaryComponent name="Overdue" noOfTask={24} color="#FB5D5D" accentColor="#330101" logo={<MdOutlineErrorOutline />} /> */}
       </div>
     </div>
   );
