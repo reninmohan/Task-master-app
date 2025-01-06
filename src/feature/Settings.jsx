3a0f27c8ddb040149d2203e5fa1404e27f3a307e
Renin <reninmohan17@gmail.com>
2025-01-07 01:21:56 +0530

    fixed setting layout

--- src/feature/Settings.jsx
+++ src/feature/Settings.jsx
@@ -4,10 +4,7 @@ import SettingBtnGroup from "../components/SettingBtnGroup";
 function Settings({ sortedDates }) {
   return (
     <div style={{ backgroundColor: "#958B8B" }}>
-      <div
-        className="d-md-flex justify-content-md-between  mx-auto p-3 d-sm-grid justify-content-start gap-sm-2"
-        style={{ maxWidth: "85rem" }}
-      >
+      <div className="d-flex justify-content-between  mx-auto p-3 " style={{ maxWidth: "85rem" }}>
         <DateDisplay sortedDates={sortedDates} />
         <SettingBtnGroup />
       </div>
